0136efa6f8bdf5904971775562e838b3326e3f9b
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-20 16:13:11 +0500

    task13 done

--- task13.py
+++ task13.py
@@ -1,3 +1,3 @@
 def filter_long_names(students: list, min_length: int = 5) -> list:
-    return [name for name in students if len(name) > min_length]
+    return [name for name in students if len(name) >= min_length]
 print(filter_long_names(["Ali", "Vali", "Hasan", "Husan", "Aziza", "Jasurbek"]))
