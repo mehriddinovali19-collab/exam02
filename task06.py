8fc931714ba16fcb4dcb2d3d5023e72a590518df
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-20 15:30:02 +0500

    task06 done

---
+++ task06.py
@@ -0,0 +1,15 @@
+def analyze_grades(students: dict) -> dict: 
+    total = sum(students.values())
+    count = len(students)
+    average = total / count
+    
+    above_average = []
+    
+    for name, grade in students.items():
+        if grade > average:
+            above_average.append(name)
+    return {"average": average, "above_average": above_average}
+
+
+print(analyze_grades({"Ali": 5, "Vali": 4, "Hasan": 5, "Husan": 3}))
+
