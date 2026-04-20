72a8ec60d0344f9d1a0d1ac9dc7b1b2acb83abac
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-20 15:02:47 +0500

    task03 done

---
+++ task03.py
@@ -0,0 +1,20 @@
+def calculate_tax(salary: int) -> dict: 
+ 
+    if salary <= 5_000_000:
+        tax_rate = 0
+        rate_str = "0%"
+    elif salary <= 10_000_000:
+        tax_rate = 0.12
+        rate_str = "12%"
+    elif salary <= 20_000_000:
+        tax_rate = 0.18
+        rate_str = "18%"
+    else:
+        tax_rate = 0.25
+        rate_str = "25%"
+    tax = int(salary * tax_rate)
+    net = salary - tax
+    return {"gross": salary,"tax": tax,"net": net,"rate": rate_str}
+
+print(calculate_tax(8_000_000))
+print(calculate_tax(3_000_000))
