486cf5b1f0bf80adbb2b4f78b0a223411f4ede3d
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-23 20:21:55 +0500

    task14 done

---
+++ task14.py
@@ -0,0 +1,21 @@
+def find_pattern(items: list, pattern: str, match_type: str) -> list: 
+    lower_pattern = pattern.lower()
+    result =[]
+    for item in items: 
+        lower_items = item.lower()
+        if match_type == "starts" and lower_items.startswith(lower_pattern):
+            result.append(item)
+        elif match_type == "ends" and lower_items.endswith(lower_pattern):
+            result.append(item)
+        elif match_type == "contains" and lower_pattern in lower_items:
+            result.append(item)
+    return result
+print(find_pattern(["Ali", "Alisher", "Vali", "Aziz"], "A", "starts"))
+# ["Ali", "Alisher", "Aziz"]
+
+print(find_pattern(["Alisher", "Bobur", "Jasur"], "ur", "ends"))
+# ["Bobur", "Jasur"]
+
+print(find_pattern(["Python", "Java", "JavaScript"], "java", "contains"))
+# ["Java", "JavaScript"]
+
