846395cff3245e50f53959bee92a625d11367d86
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-23 23:01:22 +0500

    task17 done

--- task17.py
+++ task17.py
@@ -1,6 +1,13 @@
 def filter_positive(numbers: list) -> list:
     result = []
-    if numbers > 0:
-        result.append(numbers)
+    for num in numbers:
+        if num['value'] > 0:
+            result.append(num)
     return result
+
 print(filter_positive([{'value': -5}, {'value': 10}, {'value': -1}, {'value': 7}]))
+# [{'value': 10}, {'value': 7}]
+
+print(filter_positive([{'value': 0}, {'value': 5}, {'value': -3}]))
+# [{'value': 5}]
+
