21d107a11dd5ce8316116c55a4efdd706c5fc43a
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-23 18:58:48 +0500

    task08 done

--- task08.py
+++ task08.py
@@ -1,7 +1,5 @@
 def calculate_stats(numbers: list) -> dict:
     total = sum(numbers)
-    average = round(total / len(numbers), 2)
-    return {"sum": total,"average": average}
-
+    average= round(total/len(numbers), 2)
+    return  {'sum': total, 'average': average}
 print(calculate_stats([3, 7, 10, -5, -8, 15, 22]))
-print(calculate_stats([10, 20, 30]))
