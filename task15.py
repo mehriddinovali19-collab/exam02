b0b5084efcd6bc0738583bca1b8b900853204fac
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-23 20:39:48 +0500

    task15 done

---
+++ task15.py
@@ -0,0 +1,10 @@
+def find_top_students(grades: dict) -> dict:
+    max_grade = max(grades.values())
+
+    top_grade = []
+    for name, grade in grades.items():
+       if grade == max_grade:
+          top_grade.append(name)
+    return {"max_grade": max_grade, "students": top_grade}
+print(find_top_students({"Ali": 5, "Vali": 4, "Hasan": 3, "Husan": 5}))
+print(find_top_students({"Aziz": 4, "Bobur": 5, "Diyor": 3}))
