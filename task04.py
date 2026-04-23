f1ed2d74d11cbeaaeebd2ad740c1e75b54f18221
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-23 18:13:21 +0500

    task04 done

--- task04.py
+++ task04.py
@@ -1,6 +1,7 @@
 def format_name(full_name: str) -> str:
     parts = full_name.split()
     surname = parts[0]
-    given_names = parts[1:]
-    return " ".join(given_names) + ", " + surname
+    given_name = parts[1:]
+
+    return " ".join(given_name) + " " + surname
 print(format_name("Aliyev Vali G'aniyevich"))
